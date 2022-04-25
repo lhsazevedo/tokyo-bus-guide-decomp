c629669a360998709ad024755670169f205818d5
Lucas Azevedo <lhs_azevedo@hotmail.com>
2022-04-24 22:38:12 -0300

    Add two matching functions

---
+++ includes.h
@@ -0,0 +1,13 @@
+typedef enum {FALSE, TRUE} boolean;
+
+struct Task {
+    void (*action)(struct Task *task, void *state);
+    void *state;
+    int field_0x08;
+    int field_0x0c;
+    int field_0x10;
+    int field_0x14;
+    int field_0x18;
+    int field_0x1c;
+}
+typedef Task;
