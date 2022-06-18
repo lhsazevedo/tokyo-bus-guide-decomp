ac71c630854956a9c59c7cf8d8cf3e983553fdfe
Lucas Azevedo <lhs_azevedo@hotmail.com>
2022-06-18 12:40:41 -0300

    Include Ninja headers

--- includes.h
+++ includes.h
@@ -1,5 +1,7 @@
 typedef enum {FALSE, TRUE} boolean;
 
+#include "ninja.h"
+
 struct Task {
     void (*action)(struct Task *task, void *state);
     void *state;
@@ -15,7 +17,7 @@ typedef Task;
 struct DrawDatStruct1 {
     NJS_TEXLIST *tlist_0x00;
     NJS_TEXANIM *tanim_0x04;
-    char *contents_0x08;
+    void *contents_0x08;
     int field_0x0c;
 }
 typedef DrawDatStruct1;
