6602ba5076856ca674325ef43ce30f8f4aa9e092
Lucas Azevedo <lhs_azevedo@hotmail.com>
2023-07-04 01:43:11 -0300

    Add some useful scripts

---
+++ tools/rename_labels_ghidra.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+# TODO: Description
+
+import sys
+import re
+import fileinput
+
+if (len(sys.argv) > 2):
+    print("order_labels.py inputfile")
+    sys.exit(1)
+
+labels = []
+out = ""
+
+with fileinput.input(openhook=fileinput.hook_encoded("shift_jis")) as f:
+    for line in f:
+        m = re.search(r'^(LAB_[a-f0-9]+):', line)
+        if m:
+            labels.append(m.group(1))
+
+        out += line
+
+for i, l in enumerate(labels):
+    nl = "L" + str(i)
+    out = re.sub(r'\b' + l, nl, out)
+
+print(out)
