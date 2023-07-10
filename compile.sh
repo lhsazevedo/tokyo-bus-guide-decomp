484fcd7f4db829efce03b9bc30d8e4374ac2f3cb
Lucas Azevedo <lhs_azevedo@hotmail.com>
2023-07-10 10:44:53 -0300

    Identify sound files

--- compile.sh
+++ compile.sh
@@ -4,8 +4,8 @@ ASMSH_FLAGS="-debug=d -cpu=sh4 -endian=little -sjis"
 
 wine $SHC_BIN/asmsh.exe src\\_000000_8c010000.src -object=build\\_000000_8c010000.obj $ASMSH_FLAGS
 wine $SHC_BIN/shc.exe src\\_000128_8c010080_main.c -object=build\\_000128_8c010080_main.obj -sub=shc.sub
-wine $SHC_BIN/shc.exe src\\_000188_8c0100bc.c -object=build\\_000188_8c0100bc.obj -sub=shc.sub
-wine $SHC_BIN/asmsh.exe src\\_000572_8c01023c.src -object=build\\_000572_8c01023c.obj $ASMSH_FLAGS
+wine $SHC_BIN/shc.exe src\\_000188_8c0100bc_sound.c -object=build\\_000188_8c0100bc_sound.obj -sub=shc.sub
+wine $SHC_BIN/asmsh.exe src\\_000572_8c01023c_sound.src -object=build\\_000572_8c01023c_sound.obj $ASMSH_FLAGS
 wine $SHC_BIN/shc.exe src\\_003728_8c010e90.c -object=build\\_003728_8c010e90.obj -sub=shc.sub
 wine $SHC_BIN/asmsh.exe src\\_004072_8c010fe8.src -object=build\\_004072_8c010fe8.obj $ASMSH_FLAGS
 wine $SHC_BIN/asmsh.exe src\\_004384_8c011120.src -object=build\\_004384_8c011120.obj $ASMSH_FLAGS
