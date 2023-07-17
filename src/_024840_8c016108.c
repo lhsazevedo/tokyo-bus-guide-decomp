0da292098a43afd1625d4b5d3334a3bb7ef58922
Lucas Azevedo <lhs_azevedo@hotmail.com>
2023-07-17 02:54:37 -0300

    Rename struct

--- src/_024840_8c016108.c
+++ src/_024840_8c016108.c
@@ -2,14 +2,14 @@
 #include "_023224_8c015ab8_title.h"
 
 extern ResourceGroup* _8c2263a8;
-extern freeDrawDatStruct_8c0185c4(ResourceGroup* dds);
+extern freeResourceGroup_8c0185c4(ResourceGroup* dds);
 
 /* Matched */
-void freeDrawDatStructs_8c016108()
+void freeResourceGroups_8c016108()
 {
-    freeDrawDatStruct_8c0185c4(&menuState_8c1bc7a8.field_0x00);
-    freeDrawDatStruct_8c0185c4(&menuState_8c1bc7a8.drawDatStruct1_0x0c);
-    freeDrawDatStruct_8c0185c4(&_8c2263a8);
+    freeResourceGroup_8c0185c4(&menuState_8c1bc7a8.field_0x00);
+    freeResourceGroup_8c0185c4(&menuState_8c1bc7a8.drawDatStruct1_0x0c);
+    freeResourceGroup_8c0185c4(&_8c2263a8);
 
     _8c02af32();
     _8c225fb0 = (void *) -1;
