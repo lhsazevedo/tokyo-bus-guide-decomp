6cf218abf51890e507b02edd45e90d46c6c16bb0
Lucas Azevedo <lhs_azevedo@hotmail.com>
2023-11-09 22:17:51 -0300

    Update compile script

--- compile.sh
+++ compile.sh
@@ -13,7 +13,8 @@ wine $SHC_BIN/asmsh.exe src\\_008160_8c011fe0.src -object=build\\_008160_8c011fe
 wine $SHC_BIN/shc.exe src\\_018740_8c014934.c -object=build\\_018740_8c014934.obj -sub=shc.sub
 wine $SHC_BIN/shc.exe src\\_018864_8c0149b0_sbinit.c -object=build\\_018864_8c0149b0_sbinit.obj -sub=shc.sub
 wine $SHC_BIN/shc.exe src\\_019100_8c014a9c_tasks.c -object=build\\_019100_8c014a9c_tasks.obj -sub=shc.sub
-wine $SHC_BIN/asmsh.exe src\\_019340_8c014b8c.src -object=build\\_019340_8c014b8c.obj $ASMSH_FLAGS
+wine $SHC_BIN/shc.exe src\\_019340_8c014b8c_backup.c -object=build\\_019340_8c014b8c_backup.obj -sub=shc.sub
+wine $SHC_BIN/asmsh.exe src\\_020308_8c014f54.src -object=build\\_020308_8c014f54.obj $ASMSH_FLAGS
 wine $SHC_BIN/asmsh.exe src\\_023224_8c015ab8_title.src -object=build\\_023224_8c015ab8_title.obj $ASMSH_FLAGS
 wine $SHC_BIN/shc.exe src\\_024840_8c016108.c -object=build\\_024840_8c016108.obj -sub=shc.sub
 wine $SHC_BIN/asmsh.exe src\\_024908_8c01614c.src -object=build\\_024908_8c01614c.obj $ASMSH_FLAGS
