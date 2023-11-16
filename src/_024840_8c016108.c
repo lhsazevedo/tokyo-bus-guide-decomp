a2b850a41c64b826fd85b54014111e2f1c8dd199
Lucas Azevedo <lhs_azevedo@hotmail.com>
2023-11-16 00:45:24 -0300

    Prefix vars

--- src/_024840_8c016108.c
+++ src/_024840_8c016108.c
@@ -1,7 +1,8 @@
 #include <shinobi.h>
 #include "_023224_8c015ab8_title.h"
 
-extern ResourceGroup* _8c2263a8;
+extern void* var_8c225fb0;
+extern ResourceGroup* var_8c2263a8;
 extern freeResourceGroup_8c0185c4(ResourceGroup* dds);
 
 /* Matched */
@@ -9,8 +10,8 @@ void freeResourceGroups_8c016108()
 {
     freeResourceGroup_8c0185c4(&menuState_8c1bc7a8.field_0x00);
     freeResourceGroup_8c0185c4(&menuState_8c1bc7a8.drawDatStruct1_0x0c);
-    freeResourceGroup_8c0185c4(&_8c2263a8);
+    freeResourceGroup_8c0185c4(&var_8c2263a8);
 
     FUN_8c02af32();
-    _8c225fb0 = (void *) -1;
+    var_8c225fb0 = (void *) -1;
 }
