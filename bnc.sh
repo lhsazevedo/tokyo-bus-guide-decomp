6dc32495afd09553f3222f9cd669c662739c170a
Lucas Azevedo <lhs_azevedo@hotmail.com>
2024-02-01 00:06:47 -0300

    Begin decompiling _027636_8c016bf4

--- bnc.sh
+++ bnc.sh
@@ -1,12 +1,12 @@
 # Build and compare
 
-wine $SHC_BIN/shc.exe src\\_067540_8c0207d4.c \
- -object=build\\_067540_8c0207d4.src \
+wine $SHC_BIN/shc.exe src\\_027636_8c016bf4.c \
+ -object=build\\_027636_8c016bf4.src \
  -code=asmcode \
  -sub=shc.sub && \
  delta -s \
- <(cat src/asm/_067540_8c0207d4.src) \
- <(cat build/_067540_8c0207d4.src | sed '/.LINE/d' | ./tools/rename_labels.py | ./tools/ljust_labels.py )
+ <(cat src/asm/_027636_8c016bf4.src) \
+ <(cat build/_027636_8c016bf4.src | sed '/.LINE/d' | ./tools/rename_labels.py | ./tools/ljust_labels.py )
 
 #  <(cat src/_004384_8c011120.src | sed '0,/^_request_dat_8c011182/d') \
 #  <(cat build/_004384_8c011120.src | sed '/.LINE/d' | ./tools/rename_labels.py | sed '0,/^_request_dat_8c011182/d')
---
+++ src/non_matching/_027636_8c016bf4.c
@@ -0,0 +1,53 @@
+#include <shinobi.h>
+// sizeof _var_8c225fa8 = 8
+
+typedef struct Struct8c225fa8 Struct8c225fa8;
+
+struct Struct8c225fa8
+{
+    Struct8c225fa8* field_0x00;
+    Sint8 field_0x04;
+    Sint8 field_0x05;
+    Sint8 field_0x06;
+    Sint8 field_0x07;
+};
+
+
+extern Struct8c225fa8* var_8c225fa8;
+extern Struct8c225fa8* var_8c225fa8_2;
+extern int var_8c1bbc84;
+//extern int var_8c1ba364;
+extern int var_8c225fac;
+extern PDS_PERIPHERAL peripheral_8c1ba35c[2];
+
+void FUN_8c016bf4()
+{
+    Struct8c225fa8* temp;
+
+    if (var_8c1bbc84 >= 1) {
+        temp = &var_8c225fa8->field_0x00;
+
+        if (temp < &var_8c225fa8_2) {
+            // var_8c1ba364 = *_var_8c225fa8;
+            peripheral_8c1ba35c[0].on = (int) &temp;
+
+            // var_8c1ba36c = var_8c1ba364 & (_var_8c225fac ^ var_8c1ba364);
+            peripheral_8c1ba35c[0].press = peripheral_8c1ba35c[0].on & (var_8c225fac ^ peripheral_8c1ba35c[0].on);
+
+            // var_8c1ba378 = (short)*(char *)(_var_8c225fa8 + 1);
+            peripheral_8c1ba35c[0].x2 = var_8c225fa8->field_0x04;
+
+            // var_8c1ba374 = (ushort)*(byte *)((int)_var_8c225fa8 + 5);
+            peripheral_8c1ba35c[0].x1 = var_8c225fa8->field_0x05;
+
+            // var_8c1ba376 = (ushort)*(byte *)((int)_var_8c225fa8 + 6);
+            peripheral_8c1ba35c[0].y1 = var_8c225fa8->field_0x06;
+
+            // _var_8c225fa8 = _var_8c225fa8 + 2;
+            var_8c225fa8++;
+
+            // _var_8c225fac = var_8c1ba364;
+            var_8c225fac = peripheral_8c1ba35c[0].on;
+        }
+    }
+}
