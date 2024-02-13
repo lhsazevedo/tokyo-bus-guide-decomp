ff9cc7db6b4ac6d5911c1ca78219c42d1ad8fd13
Lucas Azevedo <lhs_azevedo@hotmail.com>
2024-02-13 18:53:32 -0300

    Rename var

--- src/non_matching/_004072_8c010fe8.c
+++ src/non_matching/_004072_8c010fe8.c
@@ -22,31 +22,31 @@ struct Test {
     int field_0x0c;
 };
 
-// extern Test _8c157a58;
+// extern Test var_8c157a58;
 extern Test3* _8c157a5c;
 extern int aa;
 extern int ab;
 
 /* === Uninitialized vars === */
-extern Test _8c157a58;
+extern Test var_8c157a58;
 
 FUN_8c010fe8(Test2* p1, Test3* p2) {
-    _8c157a58.field_0x00 = p1;
+    var_8c157a58.field_0x00 = p1;
 
     /* 8c010ff4 */
-    _8c157a58.field_0x0c = 0;
+    var_8c157a58.field_0x0c = 0;
 
     /* 8c011004 */
-    _8c157a58.field_0x00->field_0x08 = ((-32 + (int) p2) - (int) p1) * 32;
+    var_8c157a58.field_0x00->field_0x08 = ((-32 + (int) p2) - (int) p1) * 32;
 
     /* 8c01100e */
-    _8c157a58.field_0x00->field_0x04 = p2 - 2;
+    var_8c157a58.field_0x00->field_0x04 = p2 - 2;
 
-    /* _8c157a5c = _8c157a58.field_0x00->field_0x04;
-    _8c157a58.field_0x00->field_0x04->field_0x0c = 0;
-    _8c157a58.field_0x00->field_0x04->field_0x08 = 0;
-    _8c157a58.field_0x00->field_0x04->field_0x00 = (Test3*) &_8c157a58;
-    _8c157a58.field_0x00->field_0x04->field_0x04 = 0; */
+    /* _8c157a5c = var_8c157a58.field_0x00->field_0x04;
+    var_8c157a58.field_0x00->field_0x04->field_0x0c = 0;
+    var_8c157a58.field_0x00->field_0x04->field_0x08 = 0;
+    var_8c157a58.field_0x00->field_0x04->field_0x00 = (Test3*) &var_8c157a58;
+    var_8c157a58.field_0x00->field_0x04->field_0x04 = 0; */
 
     aa = 0;
     ab = 0;
