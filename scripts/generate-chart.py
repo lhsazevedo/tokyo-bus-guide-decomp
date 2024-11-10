a6164d15a51437448d612ad5c6649677c0127bb2
Lucas Azevedo <lhs_azevedo@hotmail.com>
2024-11-10 03:17:43 -0300

    Update progress

--- scripts/generate-chart.py
+++ scripts/generate-chart.py
@@ -1,17 +1,34 @@
 import matplotlib.pyplot as plt
 from datetime import datetime
 
-dates = [datetime(2022, 3, 27), datetime(2024, 1, 1), datetime(2024, 7, 19), datetime(2024, 11, 1), datetime(2024, 11, 5)]
+dates = [
+    datetime(2022, 3, 27),
+    datetime(2024, 1, 1),
+    datetime(2024, 7, 19),
+    datetime(2024, 11, 1),
+    datetime(2024, 11, 5),
+    datetime(2024, 11, 10)
+]
+
 decompiled_code = [
-    0.0, 2.0, 12.6, 14.7, 14.9
+    0.0,
+    2.0,
+    12.6,
+    14.7,
+    14.9,
+    15.2
 ]
 matched_code = [
-    0.0, 1.0, 1.3, 1.3, 1.3
+    0.0,
+    1.0,
+    1.3,
+    1.3,
+    1.3,
+    1.3
 ]
 
 fig, ax = plt.subplots()
 
-# Using vibrant colors and highlighting Decompiled Code
 ax.plot(dates, decompiled_code, label='Decompiled Code', color='#4659eb', marker='.', markersize=8, linewidth=2.5)
 ax.plot(dates, matched_code, label='Matched Code', color='#5a6ae6', marker='.', markersize=8, linestyle='--', linewidth=1.5)
 
